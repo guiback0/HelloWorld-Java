950b52c3d7dd6bc2008c8dd2083e322783817f5a
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-06-02 14:51:39 +0200

    first commit

---
+++ src/com/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i < 5; i++) {
+			System.out.println("Hello World !");
+		}
+
+		System.out.println("Fin du programme !");
+	}
+
+}
